3daf52949d6c8bd0de80e2f0cd3d4b5fb0f88724
xii <xiiforlearning@gmail.com>
2020-10-18 01:21:33 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const arr = n.toString().split('').reverse()
+    if (arr[arr.length - 1] === '-') {
+       const result = arr.slice(0, -1).join('')
+       return Number(result)
+    }
+    return Number(arr.join(''))
 }
